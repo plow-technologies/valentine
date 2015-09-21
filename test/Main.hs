6d54d65eb2b93d3b04e3084c4abed6ad8fafc4bb
Michael <michael.sewell@plowtech.net>
2015-09-21 15:55:06 -0500

    added some tests to test suite

--- test/Main.hs
+++ test/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import Valentine.ParserSpec
 import Data.Either (lefts)
-
+import Data.List (null)
 main :: IO ()
-main = print . lefts $ allTestSuites
+main = if null allTestSuites
+       then print ("All Tests Passed" :: String)
+       else (putStrLn . unwords $ allTestSuites )*>
+            fail ("Error, some tests failed" :: String)
  where
-   allTestSuites = concat [valentineParserSpec]
+   allTestSuites = lefts.concat $ [valentineParserSpec] ::[String]
--- test/Valentine/ParserSpec.hs
+++ test/Valentine/ParserSpec.hs
@@ -1,24 +1,58 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Valentine.ParserSpec where
 
 import Valentine.Parser.VDom (parseVNodeS)
 
-
+import Data.Monoid ((<>))
 import Control.Lens 
+import           Text.Trifecta.Result
+import           LiveVDom.Adapter.Types
+import Language.Haskell.TH (nameBase)
 
-type ValidTest a =  (String, a -> Bool)
+makeLensesFor [(nameBase 'vNodePropsList , "_" <> nameBase 'vNodePropsList) ] ''VNodeAdapter 
+makePrisms ''VNodeAdapter
 
-assert :: ValidTest a  -> a -> Either String Bool
-assert (str, f) a = if f a
+type ValidTest =  (String, Bool)
+
+assert :: ValidTest -> Either String Bool
+assert (str, rslt)  = if rslt
                   then Right True
                   else Left str
 
 valentineParserSpec :: [Either String Bool]
-valentineParserSpec = assert <$> allTests <*> inputs
+valentineParserSpec = assert <$> allTests 
  where
-  allTests = [] :: [ValidTest a]
-  inputs = []
+  allTests = [ testParseVNodeS
+             , testDontParseBrokenVNodeS
+             , testParseVNodeWithProp] 
+
+
+-- Inputs
+simpleDivDiv = "<div class=\"panel\"> \n  here is a parsed thing with not much going on"
+simpleDivDivBroken = "<div here is a parsed thing with not much going on"
+
+
+-- Test General Parsing Property
+
+
+testParseVNodeS :: ValidTest 
+testParseVNodeS = ("Error Parsing Vnode on " <> simpleDivDiv , vnodeParserTester simpleDivDiv)
+ where
+  vnodeParserTester str = maybe False (const True) $ parseVNodeS str ^? _Just . _Success
+
+
+testDontParseBrokenVNodeS :: ValidTest 
+testDontParseBrokenVNodeS = ("Error Parsing Vnode on " <> simpleDivDivBroken , vnodeParserTester simpleDivDivBroken)
+ where
+  vnodeParserTester str = maybe True (const False) $ parseVNodeS str ^? _Just . _Success
 
 
 
-testParseVNodeS = undefined 
 
+
+
+testParseVNodeWithProp :: ValidTest 
+testParseVNodeWithProp = ("Error Parsing Vnode class on " <> simpleDivDiv , vnodeParserTester simpleDivDiv)
+ where
+  vnodeParserTester str = not $ nullOf ( _Just . _Success .folded . _vNodePropsList ) (parseVNodeS str)
