4f982b30715c84d2459336328da073ac4ffe7563
James Haver <mchaver@gmail.com>
2015-09-23 11:14:35 +0800

    Remove canvas from rebuildSandbox, update live-vdom submodule

--- rebuildSandbox.sh
+++ rebuildSandbox.sh
@@ -3,7 +3,6 @@ cabal sandbox init
 
 cabal sandbox add-source non-hackage-dependencies/live-vdom
 cabal sandbox add-source non-hackage-dependencies/live-vdom/non-hackage-dependencies/ghcjs-base
-cabal sandbox add-source non-hackage-dependencies/live-vdom/non-hackage-dependencies/ghcjs-canvas
 cabal sandbox add-source non-hackage-dependencies/live-vdom/non-hackage-dependencies/ghcjs-ffiqq
 cabal sandbox add-source non-hackage-dependencies/live-vdom/non-hackage-dependencies/ghcjs-vdom
 cabal sandbox add-source non-hackage-dependencies/stm-notify
