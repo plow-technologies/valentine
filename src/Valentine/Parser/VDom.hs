ca3dde9c00a28165f611d9ec2cb4166c53ab752b
Kevin Cotrone <kevincotrone@gmail.com>
2016-01-08 16:21:01 -0600

    Updateing for no vnodeadapter

--- src/Valentine/Parser/VDom.hs
+++ src/Valentine/Parser/VDom.hs
@@ -27,43 +27,43 @@ import           Valentine.Parser
 import           Data.JSString          (JSString)
 import qualified Data.JSString          as JS (pack, unpack)
 
--- | Parse a VNode string in the form of:
--- <div property="some string" or="4">
---    <p with="child nodes">
---      And text!
-parseVNodeS :: (Applicative f, Monad f) => String -> f (Result [VNodeAdapter])
-parseVNodeS = parseStringTrees parseVNodeAdapter
+-- -- | Parse a VNode string in the form of:
+-- -- <div property="some string" or="4">
+-- --    <p with="child nodes">
+-- --      And text!
+-- parseVNodeS :: (Applicative f, Monad f) => String -> f (Result [VNodeAdapter])
+-- parseVNodeS = parseStringTrees parseVNodeAdapter
 
--- | The function that's used to parse a string tree
--- This is how the vnode parser is seperated
--- because it parsed a vnode and then tries to parse vtext
-parseVNodeAdapter :: (Monad m) => Parser ([VNodeAdapter] -> m VNodeAdapter)
-parseVNodeAdapter = parseVNode <|> parseVText
+-- -- | The function that's used to parse a string tree
+-- -- This is how the vnode parser is seperated
+-- -- because it parsed a vnode and then tries to parse vtext
+-- parseVNodeAdapter :: (Monad m) => Parser ([VNodeAdapter] -> m VNodeAdapter)
+-- parseVNodeAdapter = parseVNode <|> parseVText
 
 
--- | Parse a single property and then give a function to add
--- a number of chid vnodes. The operation is monadic because the vtext portion
--- can fail
-parseVNode :: (Monad m) => Parser ([VNodeAdapter] -> m VNodeAdapter)
-parseVNode = angles $ do
-  tagName <- parseTagName
-  props <- many parseAttribute
-  (return $ \children -> return $ VNode [] tagName props children) <?> "VNode"
+-- -- | Parse a single property and then give a function to add
+-- -- a number of chid vnodes. The operation is monadic because the vtext portion
+-- -- can fail
+-- parseVNode :: (Monad m) => Parser ([VNodeAdapter] -> m VNodeAdapter)
+-- parseVNode = angles $ do
+--   tagName <- parseTagName
+--   props <- many parseAttribute
+--   (return $ \children -> return $ VNode [] tagName props children) <?> "VNode"
 
 
 parseTagName :: Parser String
 parseTagName = manyTill alphaNum (space <|> (lookAhead $ char '>'))
 
 
--- | Parse a single text element
--- If any VNode is passed in as a child the parser fails
-parseVText :: (Monad m) => Parser ([VNodeAdapter] -> m VNodeAdapter)
-parseVText = do
-  xs <- many anyChar
-  (return $ \vns -> F.foldlM addVText (VText [] (JS.pack xs)) vns) <?> "VText"
-  where addVText (VText _ accum) (VText _ new) = return $ VText [] $ accum <> "\n" <> new
-        addVText _ (VNode _ev tn props _ch) = fail [i| Unable to add node ${show tn ++ " " ++ show props} to text as a node|]
-        addVText _ _ = fail [i|Error, somehow parsed VNode instead of VText. Please report this as a bug|]
+-- -- | Parse a single text element
+-- -- If any VNode is passed in as a child the parser fails
+-- parseVText :: (Monad m) => Parser ([VNodeAdapter] -> m VNodeAdapter)
+-- parseVText = do
+--   xs <- many anyChar
+--   (return $ \vns -> F.foldlM addVText (VText [] (JS.pack xs)) vns) <?> "VText"
+--   where addVText (VText _ accum) (VText _ new) = return $ VText [] $ accum <> "\n" <> new
+--         addVText _ (VNode _ev tn props _ch) = fail [i| Unable to add node ${show tn ++ " " ++ show props} to text as a node|]
+--         addVText _ _ = fail [i|Error, somehow parsed VNode instead of VText. Please report this as a bug|]
 
 
 
